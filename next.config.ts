b19c8fc9e5e6e3480ab97c899d5bc5bc096e290f
appyem-de-github <gerenteappyempresa@gmail.com-en-github.com>
2026-02-28 19:30:04 -0500

    Corregir next.config.ts para Netlify

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   output: 'standalone',
+  devIndicators: false,
   experimental: {
     serverActions: {
       bodySizeLimit: '2mb',
     },
   },
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
